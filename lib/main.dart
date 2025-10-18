cb58b900836cc0be211fe82a10cf1cd322500b42
berkay.cayli <berkaydiyebiri@gmail.com>
2025-10-18 16:28:07 +0300

    add: dependency injection and its generator

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:marti_case/app.dart';
+import 'package:marti_case/core/di/injection.dart';
 
 void main() {
+  configureDependencies();
   runApp(const MyApp());
 }
